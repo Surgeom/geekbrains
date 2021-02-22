be57efc2358d3744fe12407f0412e9facf522fd4
Surgeom <ushakevichkirill@gmail.com>
2021-02-22 22:18:33 +0300

    dz3+4

--- algoritms/dz3/3.py
+++ algoritms/dz3/3.py
@@ -4,3 +4,4 @@ sstr = 'papa'
 arr_of_hash = list(
     map(hashlib.sha256, set(sstr[i:j + 1].encode() for i in range(len(sstr)) for j in range(i, len(sstr)))))
 print([x.hexdigest() for x in arr_of_hash])
+print(set(sstr[i:j + 1] for i in range(len(sstr)) for j in range(i, len(sstr))))
--- algoritms/dz3/4.py
+++ algoritms/dz3/4.py
@@ -1,9 +1,9 @@
+import hashlib
 cache = {}
 url = 'https://geekbrains.ru'
 
 
 def urls_cache(url):
-    import hashlib
     salt = 'something'
     urls_hash = hashlib.sha256(salt.encode() + f'{url}'.encode()).hexdigest()
     if urls_hash not in cache.values():
