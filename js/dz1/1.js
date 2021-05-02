787286342fa21c96629418e29dfc9f2f085280fa
Surgeom <ushakevichkirill@gmail.com>
2021-05-02 23:41:13 +0300

    dz1

---
+++ js/dz1/1.js
@@ -0,0 +1,11 @@
+var t_grad = prompt('Введите температуру в градусах Цельсия', '');
+
+t_far = t_grad * 1.8 + 32;
+
+alert(t_far);
+var admin, name;
+name = 'Василий';
+admin = name;
+
+alert(admin);
+// 3 1000103
